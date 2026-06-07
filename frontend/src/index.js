4dae714300a88e3507c4c12cbb34cd623ccdf2b3
karumanchi <karumanchi@gmail.com>
2026-06-07 12:21:05 +0400

    Add frontend React index.js setup

---
+++ frontend/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+const App=()=> <h1>Worldcup Interface</h1>
+ReactDOM.render(<App/>,document.getElementById('root'));
